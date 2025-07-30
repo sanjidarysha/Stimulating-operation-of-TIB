d0bcedb9833e93e3b728cdf8e98f6fb1bdabaf06
sanjidarysha <sanjidajahanrysha@gmail.com>
2025-07-30 18:10:26 +0600

    updated model class,controller class

---
+++ src/main/java/modulepkg/tib/m1_sanjida_2312257/MaintainevidenceController.java
@@ -0,0 +1,7 @@
+package modulepkg.tib.m1_sanjida_2312257;
+
+public class MaintainevidenceController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ src/main/java/modulepkg/tib/m1_sanjida_2312257/ReviewpubliccomplaintController.java
@@ -0,0 +1,7 @@
+package modulepkg.tib.m1_sanjida_2312257;
+
+public class ReviewpubliccomplaintController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
