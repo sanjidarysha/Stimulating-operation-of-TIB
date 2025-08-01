126a318bdc6870379199c032b1412f32da189ede
Mohima_Ahmed_Sneha <mohimaahmedsneha@gmail.com>
2025-08-01 19:02:26 +0600

    Mohima_FXML_Part

---
+++ src/main/java/modulepkg/tib/m1_mohima_2110887/CommunityOfficerDashboardController.java
@@ -0,0 +1,53 @@
+package modulepkg.tib.m1_mohima_2110887;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+public class CommunityOfficerDashboardController
+{
+    @javafx.fxml.FXML
+    private TextArea infoTA;
+    @javafx.fxml.FXML
+    private Label dashboardL;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void resourcesOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void signOutOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ourGoalsOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void volunteerOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void submitOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void youthOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void eventOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void reportOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void followOA(ActionEvent actionEvent) {
+    }
+}
