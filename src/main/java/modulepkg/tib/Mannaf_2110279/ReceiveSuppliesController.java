6e7af0f02d714e25d95299349d6e0b65ec24913d
MannafZaman <mannafzaman21@gmail.com>
2025-08-14 13:15:19 +0600

    initial commit

---
+++ src/main/java/modulepkg/tib/Mannaf_2110279/ReceiveSuppliesController.java
@@ -0,0 +1,33 @@
+package modulepkg.tib.Mannaf_2110279;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class ReceiveSuppliesController
+{
+    @javafx.fxml.FXML
+    private TextField itemNameTf;
+    @javafx.fxml.FXML
+    private TextField supplierContactTF;
+    @javafx.fxml.FXML
+    private TextField unitPriceTF;
+    @javafx.fxml.FXML
+    private TextField sourceTF;
+    @javafx.fxml.FXML
+    private DatePicker dateDP;
+    @javafx.fxml.FXML
+    private TextField quantityTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void saveOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void resetOA(ActionEvent actionEvent) {
+    }
+}
