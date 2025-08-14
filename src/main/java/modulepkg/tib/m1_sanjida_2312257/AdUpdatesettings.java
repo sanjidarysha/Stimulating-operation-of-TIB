04f35e1795b27e14e87e94cdd7cf25d31b39b641
sanjidarysha <sanjidajahanrysha@gmail.com>
2025-08-14 12:51:10 +0600

    updated settings system

--- src/main/java/modulepkg/tib/m1_sanjida_2312257/AdUpdatesettings.java
+++ src/main/java/modulepkg/tib/m1_sanjida_2312257/AdUpdatesettings.java
@@ -1,4 +1,43 @@
 package modulepkg.tib.m1_sanjida_2312257;
 
-public class AdUpdatesettings {
+public class AdUpdatesettings {private String currentp,newp,confirmp;
+
+    public AdUpdatesettings(String currentp, String newp, String confirmp) {
+        this.currentp = currentp;
+        this.newp = newp;
+        this.confirmp = confirmp;
+    }
+
+    public String getCurrentp() {
+        return currentp;
+    }
+
+    public void setCurrentp(String currentp) {
+        this.currentp = currentp;
+    }
+
+    public String getNewp() {
+        return newp;
+    }
+
+    public void setNewp(String newp) {
+        this.newp = newp;
+    }
+
+    public String getConfirmp() {
+        return confirmp;
+    }
+
+    public void setConfirmp(String confirmp) {
+        this.confirmp = confirmp;
+    }
+
+    @Override
+    public String toString() {
+        return "AdUpdatesettings{" +
+                "currentp='" + currentp + '\'' +
+                ", newp='" + newp + '\'' +
+                ", confirmp='" + confirmp + '\'' +
+                '}';
+    }
 }
--- src/main/java/modulepkg/tib/m1_sanjida_2312257/AdUpdatesettingsController.java
+++ src/main/java/modulepkg/tib/m1_sanjida_2312257/AdUpdatesettingsController.java
@@ -1,7 +1,61 @@
 package modulepkg.tib.m1_sanjida_2312257;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import modulepkg.tib.common.SceneSwitcher;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class AdUpdatesettingsController
 {
     @javafx.fxml.FXML
-    public void initialize() {
-    }}
+    private PasswordField newP;
+    @javafx.fxml.FXML
+    private Label label;
+    @javafx.fxml.FXML
+    private PasswordField confirmP;
+    @javafx.fxml.FXML
+    private PasswordField currentP;
+
+
+    @javafx.fxml.FXML
+    public void Save(ActionEvent actionEvent) throws IOException {
+        FileWriter writer=new FileWriter("save password.txt");
+        writer.write("current password:"+currentP.getText()+"\n");
+        writer.write("new password:"+newP.getText()+"\n");
+        writer.write("confirm password:"+confirmP.getText()+"\n");
+        writer.write(".............");
+        writer.close();
+        label.setText("file saved");
+    }
+
+    @javafx.fxml.FXML
+    public void back(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("m1_sanjida_2312257/io_dashboard");
+    }
+
+    @javafx.fxml.FXML
+    public void add(ActionEvent actionEvent) throws IOException {
+        if(currentP.getText().isEmpty()||newP.getText().isEmpty()||confirmP.getText().isEmpty()){
+            label.setText("password empty");
+            return;
+        }
+        if(newP.getLength()<6 ){
+            label.setText(" New password must be at least 6 characters");
+            return;
+        }
+        if(confirmP.getLength()!=newP.getLength()){
+            label.setText("Invalid password");
+            return;
+        }
+        String currentp=currentP.getText();
+        String newp= newP.getText();
+        String confirmp= confirmP.getText();
+        AdUpdatesettings settings=new AdUpdatesettings(currentp,newp,confirmp);
+        label.setText("password changed");
+
+    }
+
+}
