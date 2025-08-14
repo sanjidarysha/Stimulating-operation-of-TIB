4a49695b7557b3f7b3879fa90922981d9e722903
MannafZaman <mannafzaman21@gmail.com>
2025-08-14 20:32:06 +0600

    initial commit

---
+++ src/main/java/modulepkg/tib/Mannaf_2110279/CheckStockLevelsController.java
@@ -0,0 +1,29 @@
+package modulepkg.tib.Mannaf_2110279;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class CheckStockLevelsController
+{
+    @javafx.fxml.FXML
+    private TableColumn itemNameColumn;
+    @javafx.fxml.FXML
+    private TableColumn catagoryColumn;
+    @javafx.fxml.FXML
+    private TableView stocksTV;
+    @javafx.fxml.FXML
+    private TableColumn QuantityColumn;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void downloadlistOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void markfororderOA(ActionEvent actionEvent) {
+    }
+}
