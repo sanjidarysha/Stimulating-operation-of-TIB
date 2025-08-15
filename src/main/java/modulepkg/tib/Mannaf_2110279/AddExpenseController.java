b0142dfde2486cd309f9cb8ca50081e271a42119
MannafZaman <mannafzaman21@gmail.com>
2025-08-15 20:55:41 +0600

    initial commit

---
+++ src/main/java/modulepkg/tib/Mannaf_2110279/AddExpenseController.java
@@ -0,0 +1,40 @@
+package modulepkg.tib.Mannaf_2110279;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+
+public class AddExpenseController
+{
+    @javafx.fxml.FXML
+    private ComboBox expensetypeCB;
+    @javafx.fxml.FXML
+    private Spinner amountSpinner;
+    @javafx.fxml.FXML
+    private TextArea reasonTA;
+    @javafx.fxml.FXML
+    private DatePicker dateDP;
+    @javafx.fxml.FXML
+    private ImageView addnewexpenseIV;
+    @javafx.fxml.FXML
+    private ComboBox paymentmethodCB;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void cancelOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void submitOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void uploadReceiptOA(ActionEvent actionEvent) {
+    }
+}
