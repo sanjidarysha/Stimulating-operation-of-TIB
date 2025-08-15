4a903dbe23d54a95fb61693828cb6273db7a8563
MannafZaman <mannafzaman21@gmail.com>
2025-08-15 21:23:13 +0600

    initial commit

---
+++ src/main/java/modulepkg/tib/Mannaf_2110279/ViewMonthlySpendingController.java
@@ -0,0 +1,46 @@
+package modulepkg.tib.Mannaf_2110279;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ViewMonthlySpendingController
+{
+    @javafx.fxml.FXML
+    private TableColumn noteColumn;
+    @javafx.fxml.FXML
+    private TableColumn catagoryColumn;
+    @javafx.fxml.FXML
+    private ComboBox monthCB;
+    @javafx.fxml.FXML
+    private ComboBox yearCB;
+    @javafx.fxml.FXML
+    private TableColumn differenceColumn;
+    @javafx.fxml.FXML
+    private TableView reportTV;
+    @javafx.fxml.FXML
+    private TableColumn givenBudgetColumn;
+    @javafx.fxml.FXML
+    private TableColumn amountSpentColumn;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void exportExcelOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void exportPDFOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void shareReportOA(ActionEvent actionEvent) {
+    }
+}
