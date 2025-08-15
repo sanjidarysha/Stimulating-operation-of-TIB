70a5dc591d8f3a70507b568fb86e21ee67b7d4be
MannafZaman <mannafzaman21@gmail.com>
2025-08-15 21:36:15 +0600

    initial commit

---
+++ src/main/java/modulepkg/tib/Mannaf_2110279/ManageBankTransactionsController.java
@@ -0,0 +1,37 @@
+package modulepkg.tib.Mannaf_2110279;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class ManageBankTransactionsController
+{
+    @javafx.fxml.FXML
+    private ImageView bankTransactionIV;
+    @javafx.fxml.FXML
+    private TextField accountNumberTF;
+    @javafx.fxml.FXML
+    private ComboBox transactiontypeCB;
+    @javafx.fxml.FXML
+    private DatePicker transactiondateDP;
+    @javafx.fxml.FXML
+    private TextField amountTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void cancelOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void submitOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void bankslipOA(ActionEvent actionEvent) {
+    }
+}
