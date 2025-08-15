95fcdbcf1562777a5f9f0e7d1d61893f5d464b57
MannafZaman <mannafzaman21@gmail.com>
2025-08-15 20:39:38 +0600

    initial commit

---
+++ src/main/java/modulepkg/tib/Mannaf_2110279/GenerateItemReportController.java
@@ -0,0 +1,47 @@
+package modulepkg.tib.Mannaf_2110279;
+
+import javafx.event.ActionEvent;
+import javafx.scene.chart.AreaChart;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.image.ImageView;
+
+public class GenerateItemReportController
+{
+    @javafx.fxml.FXML
+    private RadioButton byRoomRB;
+    @javafx.fxml.FXML
+    private DatePicker toDP;
+    @javafx.fxml.FXML
+    private RadioButton byDepartmentRB;
+    @javafx.fxml.FXML
+    private ImageView generateItemReportIV;
+    @javafx.fxml.FXML
+    private RadioButton byEmployeeRB;
+    @javafx.fxml.FXML
+    private AreaChart barChartAC;
+    @javafx.fxml.FXML
+    private DatePicker fromDP;
+    @javafx.fxml.FXML
+    private RadioButton byConditionRB;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void exportExcekOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void downloadpdfOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sendToAdminOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void generateReportOA(ActionEvent actionEvent) {
+    }
+}
