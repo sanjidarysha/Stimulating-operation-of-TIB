ff24bb650e265d3403b50e092c028f5ba6024ea0
MannafZaman <mannafzaman21@gmail.com>
2025-08-15 20:11:56 +0600

    initial commit

---
+++ src/main/java/modulepkg/tib/Mannaf_2110279/ViewRequestHistoryController.java
@@ -0,0 +1,65 @@
+package modulepkg.tib.Mannaf_2110279;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class ViewRequestHistoryController
+{
+    @javafx.fxml.FXML
+    private ImageView requestHistoryIV;
+    @javafx.fxml.FXML
+    private TableColumn itemNameColumn;
+    @javafx.fxml.FXML
+    private TableColumn remarksColumn;
+    @javafx.fxml.FXML
+    private TableColumn requestDateColumn;
+    @javafx.fxml.FXML
+    private TextField employeeNameTF;
+    @javafx.fxml.FXML
+    private TableColumn statusColumn;
+    @javafx.fxml.FXML
+    private DatePicker selectDateDP;
+    @javafx.fxml.FXML
+    private TextArea noteTA;
+    @javafx.fxml.FXML
+    private TableColumn employeeColumn;
+    @javafx.fxml.FXML
+    private TableColumn processedByColumn;
+    @javafx.fxml.FXML
+    private TableColumn departmentColumn;
+    @javafx.fxml.FXML
+    private TableColumn QuantityColumn;
+    @javafx.fxml.FXML
+    private TableColumn idColumn;
+    @javafx.fxml.FXML
+    private TableColumn approvalColumn;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void applyFilterOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewDetailsOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void closeOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void exportpdfOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void historyTV(Event event) {
+    }
+}
