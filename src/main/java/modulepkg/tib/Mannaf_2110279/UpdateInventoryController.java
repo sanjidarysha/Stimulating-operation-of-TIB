2a259ba43de0114d7f6adc6e01315165415ba20f
MannafZaman <mannafzaman21@gmail.com>
2025-08-15 19:26:17 +0600

    initial commit

---
+++ src/main/java/modulepkg/tib/Mannaf_2110279/UpdateInventoryController.java
@@ -0,0 +1,28 @@
+package modulepkg.tib.Mannaf_2110279;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextArea;
+
+public class UpdateInventoryController
+{
+    @javafx.fxml.FXML
+    private ComboBox selectItemCB;
+    @javafx.fxml.FXML
+    private Spinner quantityGivenSpinner;
+    @javafx.fxml.FXML
+    private TextArea updateReasonTA;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void cancelOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void saveUpdateOA(ActionEvent actionEvent) {
+    }
+}
