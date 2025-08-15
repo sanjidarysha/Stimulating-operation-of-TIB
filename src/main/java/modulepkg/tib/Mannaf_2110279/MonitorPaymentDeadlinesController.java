0906c7e736330c389586fa8f9685a8831b9822b8
MannafZaman <mannafzaman21@gmail.com>
2025-08-16 03:13:05 +0600

    initial commit

---
+++ src/main/java/modulepkg/tib/Mannaf_2110279/MonitorPaymentDeadlinesController.java
@@ -0,0 +1,40 @@
+package modulepkg.tib.Mannaf_2110279;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class MonitorPaymentDeadlinesController
+{
+    @javafx.fxml.FXML
+    private TableColumn dueDateColumn;
+    @javafx.fxml.FXML
+    private TableColumn reminderSetNewColumn;
+    @javafx.fxml.FXML
+    private TableColumn paymentIDcolumn;
+    @javafx.fxml.FXML
+    private TableColumn statusColumn;
+    @javafx.fxml.FXML
+    private TableColumn amountColumn;
+    @javafx.fxml.FXML
+    private TableColumn paymentTypeNewColumn;
+    @javafx.fxml.FXML
+    private CheckBox addReminderCB;
+    @javafx.fxml.FXML
+    private TableColumn vendorColumn;
+    @javafx.fxml.FXML
+    private TableView paymentTV;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void doneOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void exportOA(ActionEvent actionEvent) {
+    }
+}
