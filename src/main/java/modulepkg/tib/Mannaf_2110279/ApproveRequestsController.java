807a06a56db6d8c55b9c465ae0e47eea5fb3ae13
MannafZaman <mannafzaman21@gmail.com>
2025-08-15 19:15:48 +0600

    initial commit

---
+++ src/main/java/modulepkg/tib/Mannaf_2110279/ApproveRequestsController.java
@@ -0,0 +1,34 @@
+package modulepkg.tib.Mannaf_2110279;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+
+public class ApproveRequestsController
+{
+    @javafx.fxml.FXML
+    private TableColumn quantityColumn;
+    @javafx.fxml.FXML
+    private TextArea commentsTA;
+    @javafx.fxml.FXML
+    private TableColumn itemColumn;
+    @javafx.fxml.FXML
+    private TableColumn dateColumn;
+    @javafx.fxml.FXML
+    private TableColumn employeenameColumn;
+    @javafx.fxml.FXML
+    private TableView staffReqTV;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void approveOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void rejectOA(ActionEvent actionEvent) {
+    }
+}
