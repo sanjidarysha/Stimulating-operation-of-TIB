e73cebc4296e3cbee85fa2e6a16273443fab59c8
MannafZaman <mannafzaman21@gmail.com>
2025-08-15 21:13:12 +0600

    initial commit

---
+++ src/main/java/modulepkg/tib/Mannaf_2110279/ApproveBudgetRequestsController.java
@@ -0,0 +1,37 @@
+package modulepkg.tib.Mannaf_2110279;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ApproveBudgetRequestsController
+{
+    @javafx.fxml.FXML
+    private TableColumn approvedAmountColumn;
+    @javafx.fxml.FXML
+    private TableColumn priorityLevelColumn;
+    @javafx.fxml.FXML
+    private TableView budgetreqTV;
+    @javafx.fxml.FXML
+    private TableColumn statusColumn;
+    @javafx.fxml.FXML
+    private TableColumn requestIDColumn;
+    @javafx.fxml.FXML
+    private TableColumn departmentColumn;
+    @javafx.fxml.FXML
+    private TableColumn requestdateColumn;
+    @javafx.fxml.FXML
+    private TableColumn requestedAmountColumn;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void approveOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void rejectOA(ActionEvent actionEvent) {
+    }
+}
