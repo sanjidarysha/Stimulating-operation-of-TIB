0d8ce143e73724bcc4fc19486869165405d6d967
MannafZaman <mannafzaman21@gmail.com>
2025-08-15 19:34:54 +0600

    initial commit

---
+++ src/main/java/modulepkg/tib/Mannaf_2110279/RemoveReplaceItemsController.java
@@ -0,0 +1,29 @@
+package modulepkg.tib.Mannaf_2110279;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+
+public class RemoveReplaceItemsController
+{
+    @javafx.fxml.FXML
+    private ListView itemListLV;
+    @javafx.fxml.FXML
+    private DatePicker selectDateDP;
+    @javafx.fxml.FXML
+    private RadioButton removeRB;
+    @javafx.fxml.FXML
+    private TextArea reasonTA;
+    @javafx.fxml.FXML
+    private RadioButton replaceRB;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void submitOA(ActionEvent actionEvent) {
+    }
+}
