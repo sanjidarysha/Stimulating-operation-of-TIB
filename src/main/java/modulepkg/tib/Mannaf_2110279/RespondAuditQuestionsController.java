a27d6a48404bb562fec4067bcd3e3e0211efbd2a
MannafZaman <mannafzaman21@gmail.com>
2025-08-16 10:37:53 +0600

    initial commit

---
+++ src/main/java/modulepkg/tib/Mannaf_2110279/RespondAuditQuestionsController.java
@@ -0,0 +1,31 @@
+package modulepkg.tib.Mannaf_2110279;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class RespondAuditQuestionsController
+{
+    @javafx.fxml.FXML
+    private TextField answeredbyTF;
+    @javafx.fxml.FXML
+    private TextArea answerTA;
+    @javafx.fxml.FXML
+    private DatePicker responsedateDP;
+    @javafx.fxml.FXML
+    private ListView audirQuesLV;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void submitOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void fileuploadOA(ActionEvent actionEvent) {
+    }
+}
