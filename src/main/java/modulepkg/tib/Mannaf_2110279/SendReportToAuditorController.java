ed56c052bdb8e31710fd4916f0f3b8c33e74d57d
MannafZaman <mannafzaman21@gmail.com>
2025-08-16 10:49:10 +0600

    initial commit

---
+++ src/main/java/modulepkg/tib/Mannaf_2110279/SendReportToAuditorController.java
@@ -0,0 +1,27 @@
+package modulepkg.tib.Mannaf_2110279;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+
+public class SendReportToAuditorController
+{
+    @javafx.fxml.FXML
+    private ComboBox reporttypeCB;
+    @javafx.fxml.FXML
+    private DatePicker sendDateDP;
+    @javafx.fxml.FXML
+    private ListView selectreportLV;
+    @javafx.fxml.FXML
+    private TextArea addmessegeTA;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void sendOA(ActionEvent actionEvent) {
+    }
+}
